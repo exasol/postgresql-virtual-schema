48eaafcab57073d72e39465ba3a84c0b0ca252c6
Jakob Braun <jakob.braun@posteo.de>
2020-12-21 16:58:43 +0100

    fixed tests in PostgreSQLSqlDialectIT

--- src/test/java/com/exasol/adapter/dialects/postgresql/PostgresVirtualSchemaIntegrationTestSetup.java
+++ src/test/java/com/exasol/adapter/dialects/postgresql/PostgresVirtualSchemaIntegrationTestSetup.java
@@ -32,7 +32,7 @@ public class PostgresVirtualSchemaIntegrationTestSetup implements Closeable {
     private static final String JDBC_DRIVER_NAME = "postgresql.jar";
     static final Path JDBC_DRIVER_PATH = Path.of("target/postgresql-driver/" + JDBC_DRIVER_NAME);
     private static final String EXASOL_DOCKER_IMAGE_REFERENCE = "7.0.5";
-    private static final String POSTGRES_CONTAINER_NAME = "postgres:9.6.2";
+    private static final String POSTGRES_CONTAINER_NAME = "postgres:13.1";
     private static final String DOCKER_IP_ADDRESS = "172.17.0.1";
     private static final int POSTGRES_PORT = 5432;
     private final Statement postgresStatement;
