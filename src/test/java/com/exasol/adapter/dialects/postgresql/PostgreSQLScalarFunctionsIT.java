a14f991ae45a74d02192b6f901943de5cd324469
Jakob Braun <jakob.braun@posteo.de>
2021-01-11 12:24:37 +0100

    refactored to use test-db-builder

--- src/test/java/com/exasol/adapter/dialects/postgresql/PostgreSQLScalarFunctionsIT.java
+++ src/test/java/com/exasol/adapter/dialects/postgresql/PostgreSQLScalarFunctionsIT.java
@@ -1,30 +1,28 @@
 package com.exasol.adapter.dialects.postgresql;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import com.exasol.adapter.dialects.ScalarFunctionsAbstractIT;
 import com.exasol.closeafterall.CloseAfterAll;
 import com.exasol.closeafterall.CloseAfterAllExtension;
+import com.exasol.dbbuilder.dialects.Schema;
 import com.exasol.dbbuilder.dialects.exasol.VirtualSchema;
-import org.junit.jupiter.api.extension.ExtendWith;
-
-import java.sql.*;
-import java.util.Map;
 
 @ExtendWith({ CloseAfterAllExtension.class })
 class PostgreSQLScalarFunctionsIT extends ScalarFunctionsAbstractIT {
-    private static final String SCHEMA_POSTGRES = "schema_postgres";
     private static final String EMPTY_POSTGRES_TABLE = "empty_postgres_table";
     @CloseAfterAll
     private final PostgresVirtualSchemaIntegrationTestSetup setup = new PostgresVirtualSchemaIntegrationTestSetup();
 
     @Override
     protected String setupDatabase() throws SQLException {
-        final Statement postgresqlStatement = this.setup.getPostgresqlStatement();
-        postgresqlStatement.executeUpdate("CREATE SCHEMA " + SCHEMA_POSTGRES);
-        postgresqlStatement
-                .executeUpdate("CREATE TABLE " + SCHEMA_POSTGRES + "." + EMPTY_POSTGRES_TABLE + "(id integer)");
-        postgresqlStatement
-                .executeUpdate("INSERT INTO " + SCHEMA_POSTGRES + "." + EMPTY_POSTGRES_TABLE + " VALUES(1);");
-        final VirtualSchema virtualSchema = this.setup.createVirtualSchema(SCHEMA_POSTGRES, Map.of());
+        final Schema postgresSchema = this.setup.getPostgresFactory().createSchema("schema_postgres");
+        postgresSchema.createTable(EMPTY_POSTGRES_TABLE, "id", "integer").insert(1);
+        final VirtualSchema virtualSchema = this.setup.createVirtualSchema(postgresSchema.getName(), Map.of());
         return virtualSchema.getFullyQualifiedName() + "." + EMPTY_POSTGRES_TABLE;
     }
 
--- src/test/java/com/exasol/adapter/dialects/postgresql/PostgresVirtualSchemaIntegrationTestSetup.java
+++ src/test/java/com/exasol/adapter/dialects/postgresql/PostgresVirtualSchemaIntegrationTestSetup.java
@@ -19,6 +19,7 @@ import com.exasol.bucketfs.Bucket;
 import com.exasol.bucketfs.BucketAccessException;
 import com.exasol.containers.ExasolContainer;
 import com.exasol.dbbuilder.dialects.exasol.*;
+import com.exasol.dbbuilder.dialects.postgres.PostgreSqlObjectFactory;
 
 /**
  * This class contains the common integration test setup for all PostgreSQL virtual schemas.
@@ -47,6 +48,7 @@ public class PostgresVirtualSchemaIntegrationTestSetup implements Closeable {
     private final AdapterScript adapterScript;
     private final ConnectionDefinition connectionDefinition;
     private final ExasolObjectFactory exasolFactory;
+    private final PostgreSqlObjectFactory postgresFactory;
     private final Connection postgresConnection;
     private int virtualSchemaCounter = 0;
 
@@ -63,6 +65,7 @@ public class PostgresVirtualSchemaIntegrationTestSetup implements Closeable {
             this.postgresStatement = this.postgresConnection.createStatement();
             this.exasolFactory = new ExasolObjectFactory(this.exasolContainer.createConnection(""));
             final ExasolSchema exasolSchema = this.exasolFactory.createSchema(SCHEMA_EXASOL);
+            this.postgresFactory = new PostgreSqlObjectFactory(this.postgresConnection);
             this.adapterScript = createAdapterScript(exasolSchema);
             final String connectionString = "jdbc:postgresql://" + DOCKER_IP_ADDRESS + ":"
                     + this.postgresqlContainer.getMappedPort(POSTGRES_PORT) + "/"
@@ -86,6 +89,10 @@ public class PostgresVirtualSchemaIntegrationTestSetup implements Closeable {
         final String content = "%scriptclass com.exasol.adapter.RequestDispatcher;\n" //
                 + "%jar /buckets/bfsdefault/default/" + VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION + ";\n";
         return schema.createAdapterScript(ADAPTER_SCRIPT_EXASOL, JAVA, content);
+    }
+
+    public PostgreSqlObjectFactory getPostgresFactory() {
+        return this.postgresFactory;
     }
 
     public Statement getPostgresqlStatement() {
