aa3aba0d68c03d656e7950175373aad214f3a995
Jakob Braun <jakob.braun@posteo.de>
2021-01-13 09:01:12 +0100

    refactored to only require one successful pass

--- src/test/java/com/exasol/adapter/dialects/postgresql/PostgreSQLScalarFunctionsIT.java
+++ src/test/java/com/exasol/adapter/dialects/postgresql/PostgreSQLScalarFunctionsIT.java
@@ -17,14 +17,6 @@ class PostgreSQLScalarFunctionsIT extends ScalarFunctionsAbstractIT {
     @CloseAfterAll
     private final PostgresVirtualSchemaIntegrationTestSetup setup = new PostgresVirtualSchemaIntegrationTestSetup();
 
-    /*
-     * private static final List<String> LITERALS_OLD = List.of("0.5", "2", "TRUE", "'a'", "DATE '2007-03-31'",
-     * "TIMESTAMP '2007-03-31 12:59:30.123'", "INTERVAL '1 12:00:30.123' DAY TO SECOND", "'POINT (1 2)'",
-     * "'LINESTRING (0 0, 0 1, 1 1)'", "'GEOMETRYCOLLECTION(POINT(2 5), POINT(3 5))'",
-     * "'POLYGON((5 1, 5 5, 9 7, 10 1, 5 1),(6 2, 6 3, 7 3, 7 2, 6 2))'",
-     * "'MULTIPOLYGON(((0 0, 0 2, 2 2, 3 1, 0 0)), ((4 6, 8 9, 12 5, 4 6), (8 6, 9 6, 9 7, 8 7, 8 6)))'",
-     * "'MULTILINESTRING((0 1, 2 3, 1 6), (4 4, 5 5))'");
-     */
     // TODO capital letters cause crash; bug?
     @Override
     protected String setupDatabase() throws SQLException {
