0837876ed023f0340e498e978d425b1b0c832f91
Christoph Pirkl <christoph.pirkl@exasol.com>
2026-07-29 14:17:10 +0200

    Update DB version

--- src/test/java/com/exasol/adapter/dialects/postgresql/PostgresVirtualSchemaIntegrationTestSetup.java
+++ src/test/java/com/exasol/adapter/dialects/postgresql/PostgresVirtualSchemaIntegrationTestSetup.java
@@ -32,7 +32,7 @@ public class PostgresVirtualSchemaIntegrationTestSetup implements Closeable {
     private static final Path PATH_TO_VIRTUAL_SCHEMAS_JAR = Path.of("target", VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION);
     private static final String SCHEMA_EXASOL = "SCHEMA_EXASOL";
     private static final String ADAPTER_SCRIPT_EXASOL = "ADAPTER_SCRIPT_EXASOL";
-    private static final String EXASOL_DOCKER_IMAGE_REFERENCE = "2025.2.1";
+    private static final String EXASOL_DOCKER_IMAGE_REFERENCE = "2026.1.0";
     // https://hub.docker.com/_/postgres
     private static final String POSTGRES_CONTAINER_NAME = "postgres:18.4";
 
